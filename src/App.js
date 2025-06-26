0cbd8ca4decfdce71d631ba15c71abdb98baac8c
Madhvendra Singh <smadhvendra80@gmail.com>
2025-06-26 13:07:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Chai from './chai'
+
+function App() {
+  return (
+    <>
+    <h1>Chai aur React</h1>
+    <Chai/>
+    </>
+  );
+} 
+
+export default App;
---
+++ src/chai.js
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h1>Hello World!!!</h1>
+    )
+}
+
+export default Chai
